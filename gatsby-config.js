e51ae1ed7124acdfab134ee055ca03dfb5c92dd0
Ruan Carvalho <ruancarvalho@gmail.com>
2018-05-30 00:13:07 -0400

    Added Bootstrap and Netlify CMS

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Web Dev Academy',
+    author: 'ruancarvalho',
+    authorLink: 'https://webdevacademy.com.br',
+    disqus: 'webdevacademycombr'// put your disqus ID here
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/examples`,
+        name: 'examples'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files'
+        ]
+      }
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ]
+}
---
+++ src/pages/blog/welcome.md
@@ -0,0 +1,8 @@
+---
+contentType: blog
+path: /tutoriais/bootstrap-4-como-comecar/
+title: "Bootstrap 4: Como Começar"
+---
+Neste tutorial, você vai aprender como começar um projeto com Bootstrap 4 e os conceitos básicos.
+
+Welcome to your new gatsby-blog. You can delete this file in `src/pages/blog/welcome.md`. If you have this site deployed on netlify, you can also hit the [admin](/admin).
