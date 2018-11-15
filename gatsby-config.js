9d1379461acd5c8b4ca59d1fa99edc99dbfd4566
Ruan Carvalho <ruancarvalho@gmail.com>
2018-11-15 08:19:42 -0400

    fix(config): removed typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,12 +78,12 @@ module.exports = {
     // },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-typography',
+    //   options: {
+    //     pathToConfigModule: 'src/utils/typography',
+    //   },
+    // },
     'gatsby-plugin-sass',
   ],
 }
