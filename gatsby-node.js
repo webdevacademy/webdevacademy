8df8e28a09ddc724026856175d178f99663c87c6
Ruan Carvalho <ruancarvalho@gmail.com>
2020-06-11 17:45:36 -0400

    feat(wpcms): changed gatsby to fetch WP content

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,120 +5,125 @@ const slash = require(`slash`)
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
+
   return new Promise((resolve, reject) => {
-    graphql(
-      `
-        {
-          allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/posts/"}}, sort: {order: ASC, fields: frontmatter___date}) {
-            edges {
-              node {
-                id
-                frontmatter {
-                  title
-                  slug
-                  categories
-                  tags
-                  video
-                }
-              }
+
+    graphql(`{
+      allWordpressPost {
+        edges {
+          node {
+            id
+            title
+            slug
+            categories {
+              slug
+            }
+            tags {
+              slug
             }
           }
         }
-      `
-    )
-    .then(result => {
+      }
+      allWordpressCategory {
+        edges {
+          node {
+            name
+            slug
+          }
+        }
+      }
+      allWordpressTag {
+        edges {
+          node {
+            name
+            slug
+          }
+        }
+      }
+    }`).then(result => {
       if (result.errors) {
         console.log(result.errors)
         reject(result.errors)
       }
 
+      /**
+       * Create all Posts
+       */
       const postTemplate = path.resolve("./src/templates/single.js")
-      const posts = result.data.allMarkdownRemark.edges
+      const posts = result.data.allWordpressPost.edges
 
       posts.forEach(({ node }, index) => {
         createPage({
-          path: `/${_.toLower(node.frontmatter.categories[0])}/${node.frontmatter.slug}/`,
+          path: `/${_.toLower(node.categories[0].slug)}/${node.slug}/`,
           component: slash(postTemplate),
           context: {
-            id: node.id, 
-            slug: node.frontmatter.slug,
+            id: node.id,
+            slug: node.slug,
             prev: index === 0 ? null : posts[index - 1],
             next: index === result.length - 1 ? null : posts[index + 1],
           },
         })
       })
 
+      /**
+       * Create Categories Pages
+       */
       const categoryTemplate = path.resolve('src/templates/category.js');
-      let categories = [];
-      _.each(posts, edge => {
-        if (_.get(edge, 'node.frontmatter.categories')) {    
-          categories = categories.concat(edge.node.frontmatter.categories);
-        } else {
-          categories = categories.concat('Default');
-        }
-      });
+      const categories = result.data.allWordpressCategory.edges;
 
-      categories = _.uniq(categories);
-
-      categories.forEach(category => {
+      categories.forEach(({ node }, index) => {
         createPage({
-          path: `/${_.toLower(category)}/`,
-          component: categoryTemplate,
+          path: `/${node.slug}/`,
+          component: slash(categoryTemplate),
           context: {
-            category,
+            category: node.name,
+            slug: node.slug,
           },
-        });
-      });
-      
-      const tagTemplate = path.resolve('src/templates/tag.js');      
-      let tags = [];      
-      _.each(posts, edge => {
-        if (_.get(edge, 'node.frontmatter.tags')) {
-          tags = tags.concat(edge.node.frontmatter.tags);            
-        } else {
-          tags = tags.concat('Default');      
-        }
-      });
-      
-      tags = _.uniq(tags);
+        })
+      })
 
-      tags.forEach(tag => {
+      /**
+       * Create Tags Pages
+       */
+      const tagTemplate = path.resolve('src/templates/tag.js');
+      const tags = result.data.allWordpressTag.edges;
+
+      tags.forEach(({ node }, index) => {
         createPage({
-          path: `/tag/${_.toLower(tag)}/`,
-          component: tagTemplate,
+          path: `/tag/${node.slug}/`,
+          component: slash(tagTemplate),
           context: {
-            tag,
+            tag: node.name,
+            slug: node.slug,
           },
-        });
-      });
-    }) // end allMarkdownRemark for Posts
+        })
+      })
 
+    }) // end Posts query
     .then(() => {
-      graphql(
-        `
-        {
-          allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/pages/"}}) {
-            edges {
-              node {
-                id
-                frontmatter {
-                  slug
-                }
-              }
+
+      graphql(`{
+        allWordpressPage {
+          edges {
+            node {
+              id
+              title
+              slug
             }
           }
         }
-      `
-      ).then(result => {
+      }`)
+      .then(result => {
         if (result.errors) {
           console.log(result.errors)
           reject(result.errors)
         }
+
         const pageTemplate = path.resolve("./src/templates/page.js")
 
-        _.each(result.data.allMarkdownRemark.edges, edge => {        
+        _.each(result.data.allWordpressPage.edges, edge => {
           createPage({
-            path: `/${edge.node.frontmatter.slug}/`,
+            path: `/${edge.node.slug}/`,
             component: slash(pageTemplate),
             context: {
               id: edge.node.id,
@@ -127,7 +132,9 @@ exports.createPages = ({ graphql, actions }) => {
         })
 
         resolve()
-      })
-    }) // end allMarkdownRemark for Pages
-  })
-}
+      }) // end allWordpressPage for Pages
+    
+    }) 
+    
+  }) // end Promise  
+} // end createPages
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 import Content from '../templates/content';
 
 const Home = (props) => {
-  const posts = props.data.allMarkdownRemark.edges
+  const posts = props.data.allWordpressPost.edges
   const siteTitle = props.data.site.siteMetadata.title
   const siteDescription = props.data.site.siteMetadata.description
 
@@ -20,34 +20,6 @@ const Home = (props) => {
           'class': 'home blog hfeed no-featured-image date-hidden'
         }}
       />
-
-      <section id="mc_embed_signup" className="content">
-        <form 
-            action="https://webdevacademy.us4.list-manage.com/subscribe/post?u=e0789ae860c6bf83f9be80aa8&amp;id=1d34bdc447" 
-            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" 
-            className="validate page-header section-inner bg-white" 
-            target="_blank">
-          <div id="mc_embed_signup_scroll" className="post-content">
-            <h2>Receba Tutoriais por Email</h2>
-            <div className="mc-field-group">
-              <input type="email" defaultValue="" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Digite seu e-mail..."/>
-            </div>
-            {/* <div className="mc-field-group">
-              <label for="mce-FNAME">Name </label>
-              <input type="text" value="" name="FNAME" className="required" id="mce-FNAME" />
-            </div> */}
-            <div id="mce-responses" className="clear">
-              <div className="response" id="mce-error-response" style={{ 'display': 'none' }}></div>
-              <div className="response" id="mce-success-response" style={{ 'display': 'none' }}></div>
-            </div>
-            <div style={{ 'position': 'absolute', 'left': '-5000px' }} aria-hidden="true">
-              <input type="text" name="b_e0789ae860c6bf83f9be80aa8_1d34bdc447" tabIndex="-1" defaultValue="" /></div>
-            <div className="clear">
-              <button type="submit" name="subscribe" id="mc-embedded-subscribe" className="button">Quero Receber!</button>
-            </div>
-          </div>
-        </form>
-      </section>
 
       <main className="content section-inner" id="content" role="main">
         <section className="posts" id="posts">
@@ -74,20 +46,20 @@ export const postsQuery = graphql`
         description
       }
     }
-    allMarkdownRemark(
-      limit: 6
-      sort: { fields: frontmatter___date, order: DESC}
-      filter: {fileAbsolutePath: {regex: "/posts/"}}
-    ){
+    allWordpressPost(limit: 6, sort: {fields: date, order: DESC}) {
       edges {
         node {
           id
-          frontmatter {
+          title
+          slug
+          jetpack_featured_media_url
+          categories {
+            name
             slug
-            title
-            featured_media
-            path
-            categories
+          }
+          tags {
+            name
+            slug
           }
         }
       }
